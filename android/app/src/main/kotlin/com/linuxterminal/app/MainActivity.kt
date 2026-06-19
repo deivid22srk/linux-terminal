16368637edd454489acac16ec24a26ca5a98eda6
deivid22srk <deivid22srk@users.noreply.github.com>
2026-06-19 16:31:01 -0300

    Fix xterm version and add native lib path support

--- android/app/src/main/kotlin/com/linuxterminal/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/linuxterminal/app/MainActivity.kt
@@ -1,5 +1,23 @@
 package com.linuxterminal.app
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "com.linuxterminal.app/native"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+                when (call.method) {
+                    "getNativeLibDir" -> {
+                        result.success(applicationInfo.nativeLibDir)
+                    }
+                    else -> result.notImplemented()
+                }
+            }
+    }
+}
